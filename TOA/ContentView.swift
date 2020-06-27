16d13fd3de5f06ea0d8d7f9d9dad36dedca48d2d
steve111mv <unlivregratuit@gmail.com>
2020-06-27 01:11:39 +0100

    hello TOA, project generated

---
+++ TOA/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  TOA
+//
+//  Created by Steven on 6/26/20.
+//  Copyright © 2020 Coolectif TOA. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("Hello TOÄ")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Initial commit !")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
