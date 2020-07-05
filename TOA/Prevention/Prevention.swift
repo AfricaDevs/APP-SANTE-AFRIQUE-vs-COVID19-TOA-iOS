42fee5e4c658ee6da2d4724dcb09277420310664
steve111mv <unlivregratuit@gmail.com>
2020-07-05 12:04:12 +0100

    Added a model for Prevention Options cell

---
+++ TOA/Prevention/Prevention.swift
@@ -0,0 +1,21 @@
+//
+//  Prevention.swift
+//  TOA
+//
+//  Created by Steven on 7/5/20.
+//  Copyright © 2020 Coolectif TOA. All rights reserved.
+//
+
+import SwiftUI
+
+struct Prevention: Identifiable, Decodable{
+
+    var id = UUID()
+    var title : String
+    var lottieFileName: String
+    
+    var getTitle : String {return title}
+    var getLottieFileName : String {return lottieFileName}
+
+}
+ 
