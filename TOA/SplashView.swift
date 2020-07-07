a54d44cc435c6418b6117bfceb3e88039d907cb2
steve111mv <unlivregratuit@gmail.com>
2020-07-07 03:35:46 +0100

    Corrected a NETWORK resource abuse. The State was changing too much within a few seconds and many network calls were made

--- TOA/SplashView.swift
+++ TOA/SplashView.swift
@@ -16,44 +16,43 @@ struct SplashView: View {
         
         GeometryReader { geometry in
             
-        VStack(alignment: .center){
-            
-            if !self.animateLogo {
-                Spacer()
-            }
-            
-            HStack{
+            VStack(alignment: .center){
                 
-                Spacer()
+                if !self.animateLogo {
+                    Spacer()
+                }
                 
-                Image("logo_round")
-                    .resizable().aspectRatio(contentMode: .fill)
-                    .frame(width:self.animateLogo ? 100 : 150, height: self.animateLogo ? 100 : 150)
-                    .shadow(color: Color.white, radius: 5, x: 0, y: 2)
-            Spacer()
-            }
-            .padding(EdgeInsets(top:  self.animateLogo ? geometry.safeAreaInsets.top + 90 : .zero, leading: .zero, bottom: geometry.safeAreaInsets.bottom + 80, trailing: .zero))
-                 
+                HStack{
+                    
+                    Spacer()
+                    
+                    Image("logo_round")
+                        .resizable().aspectRatio(contentMode: .fill)
+                        .frame(width:self.animateLogo ? 100 : 150, height: self.animateLogo ? 100 : 150)
+                        .shadow(color: Color.white, radius: 5, x: 0, y: 2)
+                    Spacer()
+                }
+                .padding(EdgeInsets(top:  self.animateLogo ? geometry.safeAreaInsets.top + 90 : .zero, leading: .zero, bottom: geometry.safeAreaInsets.bottom + 80, trailing: .zero))
+                
                 //very important to use GeometryReader to access safe arean values
-               
-               
-            
-            if self.animateLogo {
-                Spacer()
+                
+                
+                
+                if self.animateLogo {
+                    Spacer()
+                }
+                
+            }.background(Color("colorSplashBlue").opacity(1.0)).frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .leading)
+                .edgesIgnoringSafeArea(.all)
+                .onAppear {
+                    
+                    //DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    withAnimation {
+                        self.animateLogo = true
+                    }
+                    //}
             }
-        
-        }.background(Color("colorSplashBlue").opacity(1.0)).frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .leading)
-        .edgesIgnoringSafeArea(.all)
-        .onAppear {
-            
-           //DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-               withAnimation {
-                   self.animateLogo = true
-            }
-           //}
-       }
-        
-            
+               
         }
     }
 }
