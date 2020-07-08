66eeccdd463d79324fca4c329f928cfa1cc3fa5d
steve111mv <unlivregratuit@gmail.com>
2020-07-08 10:44:49 +0100

    Added an EnvironmentObject to handle the App settings

---
+++ TOA/UserSettings.swift
@@ -0,0 +1,13 @@
+//
+//  UserSettings.swift
+//  TOA
+//
+//  Created by Steven on 7/8/20.
+//  Copyright © 2020 Coolectif TOA. All rights reserved.
+//
+
+import Foundation
+class UserSettings: ObservableObject {
+    //true for default , false for Bigger
+    @Published var textSize : Bool = true
+}
