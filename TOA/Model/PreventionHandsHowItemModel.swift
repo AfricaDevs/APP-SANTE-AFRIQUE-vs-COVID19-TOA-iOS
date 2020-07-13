0c19f5d55c8b519553c09e1b6f6599498128e461
steve111mv <unlivregratuit@gmail.com>
2020-07-13 22:02:25 +0100

    Added two models to be used in Prevention Hands Washing tabs' How and When options

---
+++ TOA/Model/PreventionHandsHowItemModel.swift
@@ -0,0 +1,16 @@
+//
+//  PreventionHandsHowItemModel.swift
+//  TOA
+//
+//  Created by Steven on 7/13/20.
+//  Copyright © 2020 Coolectif TOA. All rights reserved.
+//
+
+import Foundation
+struct PreventionHandsHowItemModel : Identifiable{
+    let id = UUID()
+    var title : String
+    var imageName : String
+}
+
+
---
+++ TOA/Model/PreventionHandsWhenItemModel.swift
@@ -0,0 +1,13 @@
+//
+//  PreventionHandsWhenItemModel.swift
+//  TOA
+//
+//  Created by Steven on 7/13/20.
+//  Copyright © 2020 Coolectif TOA. All rights reserved.
+//
+
+import Foundation
+struct PreventionHandsWhenItemModel : Identifiable{
+    let id = UUID()
+    var title : String 
+}
